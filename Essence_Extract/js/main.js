2162c3a80dcee787af21ca6f62cae7eeb8985d9f
alchemist2124 <1250901577@qq.com>
2024-01-03 17:39:15 +0800

    comment section refine

--- Essence_Extract/js/main.js
+++ Essence_Extract/js/main.js
@@ -1,6 +1,3 @@
-/* const pageForm = document.getElementBy("pageForm");
-console.log(pageForm); */
-
 const getPageNum = () => {
     const currentArticle = document.getElementsByClassName("main__article")[0];
     const currentPageNum = Number(currentArticle.id);
@@ -23,8 +20,6 @@ const setPageLink = (firstPageNum, lastPageNum) => {
         previousLink.setAttribute("href", `${currentPageNum - 1}.html`);
         nextLink.setAttribute("href", `${currentPageNum + 1}.html`);
     }
-
-
 };
 
 const pageRedirect = (firstPageNum, lastPageNum) => {
@@ -67,35 +62,40 @@ const getTimeObj = () => {
         seconds: seconds
     }
     return timeObj;
-}
+};
 
 const generateCommentID = (timeObj) => {
     return `${timeObj.year}${timeObj.month}${timeObj.date}${timeObj.hours}${timeObj.minutes}${timeObj.seconds}`
-}
+};
 
 const timeStringify = (timeObj) => {
     return `${timeObj.year}-${timeObj.month}-${timeObj.date} ${timeObj.hours}:${timeObj.minutes}:${timeObj.seconds}`;
-}
+};
 
 const postComment = () => {
     const submitButton = document.getElementById("submitButton");
     const commentText = document.getElementById("commentText");
     const commentList = document.getElementById("commentList");
+    commentText.style.textAlign = "left";
 
     submitButton.onclick = () => {
         if (commentText.value) {
             let timeObj = getTimeObj();
-            const commentObj = {
+            let commentObj = {
                 text: commentText.value.trim(),
-                postTime: timeStringify(timeObj),
+                //postTime: timeStringify(timeObj),
                 updateTime: timeStringify(timeObj),
-                commentID: generateCommentID(timeObj)
+                commentID: generateCommentID(timeObj),
+                pageID: getPageNum()
             }
             localStorage.setItem(`${commentObj.commentID}`, JSON.stringify(commentObj));
             //console.log(commentText.value);
             const commentItem = document.createElement("li");
+            const buttonBox = document.createElement("div");
+            buttonBox.className = "buttonBox";
             addCommentItem(commentItem, commentObj);
             commentText.value = "";
+            commentItem.appendChild(buttonBox);
             commentList.appendChild(commentItem);
 
             const editButton = document.createElement("button");
@@ -108,40 +108,45 @@ const postComment = () => {
                     commentObj.updateTime = timeStringify(timeObj);
                     commentObj.text = editedComment.trim();
                     localStorage.setItem(`${commentObj.commentID}`, JSON.stringify(commentObj));
-                    editButton.parentNode.innerHTML = commentObj.updateTime + " " + commentObj.text;
-                    commentItem.appendChild(editButton);    
-                    commentItem.appendChild(deleteButton);
+                    editButton.parentNode.parentNode.innerHTML = commentObj.updateTime + " " + commentObj.text;
+                    const buttonBox = document.createElement("div");
+                    commentItem.appendChild(buttonBox);
+                    buttonBox.appendChild(editButton);   
+                    buttonBox.appendChild(deleteButton);
                 } else if (editedComment === ""){
                     alert("Please enter text!");
                 }
             }
-            commentItem.appendChild(editButton);
+            buttonBox.appendChild(editButton);
 
             const deleteButton = document.createElement("button")
             //editButton.href = "#";
             addDeleteButton(deleteButton, commentObj);
             deleteButton.onclick = () => {
                 localStorage.removeItem(`${commentObj.commentID}`);
-                deleteButton.parentNode.remove();
+                deleteButton.parentNode.parentNode.remove();
                 commentText.value = "";
             }
-            commentItem.appendChild(deleteButton);
+            buttonBox.appendChild(deleteButton);
         } else {
             alert("Please enter text!");
         }
     }
-}
+};
 
 const initComments = () => {
     const commentList = document.getElementById("commentList");
     for (let key in localStorage) {
         const commentItem = document.createElement("li");
-        const commentObj = JSON.parse(localStorage.getItem(key));
-        if (commentObj) {
+        const buttonBox = document.createElement("div");
+        buttonBox.className = "buttonBox";
+        let commentObj = JSON.parse(localStorage.getItem(key));
+        if (commentObj && commentObj.pageID === getPageNum()) {
             console.log(commentObj);
             addCommentItem(commentItem, commentObj);
+            commentItem.appendChild(buttonBox);
             commentList.appendChild(commentItem);
-
+ 
             const editButton = document.createElement("button");
             //deleteButton.href = "#";
             addEditButton(editButton, commentObj);
@@ -152,45 +157,47 @@ const initComments = () => {
                     commentObj.updateTime = timeStringify(timeObj);
                     commentObj.text = editedComment.trim();
                     localStorage.setItem(`${commentObj.commentID}`, JSON.stringify(commentObj));
-                    editButton.parentNode.innerHTML = commentObj.updateTime + " " + commentObj.text;
-                    commentItem.appendChild(editButton);   
-                    commentItem.appendChild(deleteButton);
+                    editButton.parentNode.parentNode.innerHTML = commentObj.updateTime + " " + commentObj.text;
+                    const buttonBox = document.createElement("div");
+                    commentItem.appendChild(buttonBox);
+                    buttonBox.appendChild(editButton);   
+                    buttonBox.appendChild(deleteButton);
                 } else if (editedComment === ""){
                     alert("Please enter text!");
                 }
             }
-            commentItem.appendChild(editButton);
+            buttonBox.appendChild(editButton);
 
             const deleteButton = document.createElement("button")
             //deleteButton.href = "#";
             addDeleteButton(deleteButton, commentObj);
             deleteButton.onclick = () => {
                 localStorage.removeItem(`${commentObj.commentID}`);
-                deleteButton.parentNode.remove();
+                deleteButton.parentNode.parentNode.remove();
             }
-            commentItem.appendChild(deleteButton);
+            buttonBox.appendChild(deleteButton);
         }
     }
-
-}
+};
 
 const addCommentItem = (commentItem, commentObj) => {
-    commentItem.innerText = commentObj.postTime + " " + commentObj.text;
+    //commentItem.innerText = commentObj.postTime + " " + commentObj.text;
+    commentItem.innerText = commentObj.updateTime + " " + commentObj.text;
     commentItem.id = commentObj.commentID;
     commentItem.className = "commentItem";
-}
+};
 
 const addDeleteButton = (deleteButton, commentObj) => {
     deleteButton.innerHTML = "Delete";
     deleteButton.id = commentObj.commentID;
     deleteButton.className = "commentButton deleteButton";
-}
+};
 
 const addEditButton = (editButton, commentObj) => {
     editButton.innerHTML = "Edit";
     editButton.id = commentObj.commentID;
     editButton.className = "commentButton editButton";
-}
+};
 
 pageSettingsInit(1,5);
 initComments();
